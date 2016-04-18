182474ca8d0ce38b47463b99a6a8cdf5311c8bdc
shenlin192 <shenlin192@gmail.com>
2016-04-18 11:26:03 +0200

    test

--- dither.py
+++ dither.py
@@ -1,4 +1,7 @@
-from skimage import io, data, color
+from skimage import io, color, filters 
 img = io.imread("lenna.png")
 img_gray = color.rgb2gray(img)
 io.imshow(img_gray)
+
+binary_image1 = filters.threshold_adaptive(img_gray, 15, 'gaussian')
+io.imshow(binary_image1)
