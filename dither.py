505245c3036c0b8654c4df666d80b6866b5c4558
Kunli <dengkunli@foxmail.com>
2016-04-18 11:23:04 +0200

    fixed threshold

--- dither.py
+++ dither.py
@@ -1,4 +1,11 @@
-from skimage import io, data, color
+from skimage import io, color, filters
 img = io.imread("lenna.png")
 img_gray = color.rgb2gray(img)
-io.imshow(img_gray)
+"""binary_image1 = filters.threshold_adaptive(img_gray, 15, 'mean')"""
+
+def fixedThresholding(img, threshold):
+    func = lambda x: 0 if x > threshold else 255
+    return filters.threshold_adaptive(img, 1, 'generic', param=func)
+
+bin_image_fixed = fixedThresholding(img_gray, 120/255)
+io.imshow(bin_image_fixed)
