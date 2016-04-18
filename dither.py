c7ad3077dafbce85fc2f2f33d7d43269a6aa822a
Kunli <dengkunli@foxmail.com>
2016-04-18 11:59:09 +0200

    random threshold

--- dither.py
+++ dither.py
@@ -7,5 +7,7 @@ img_gray = color.rgb2gray(img)
 
 
 bin_image_fixed = threshold.fixedThresholding(img_gray, 120/255)
-
 io.imshow(bin_image_fixed)
+
+bin_image_random = threshold.randomThresholding(img_gray, 0.3)
+io.imshow(bin_image_random)
