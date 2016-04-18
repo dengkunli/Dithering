6e6dfc9affd481a8d3d03f6c860a6122520013dc
Kunli <dengkunli@foxmail.com>
2016-04-18 12:17:05 +0200

    plot multiple

--- dither.py
+++ dither.py
@@ -1,13 +1,17 @@
 from skimage import io, color, filters
+import matplotlib.pyplot as plt
 import threshold
 
 
 img = io.imread("lenna.png")
 img_gray = color.rgb2gray(img)
 
-
 bin_image_fixed = threshold.fixedThresholding(img_gray, 120/255)
-io.imshow(bin_image_fixed)
+plt.figure(1)
+plt.imshow(bin_image_fixed, cmap='Greys_r')
+
 
 bin_image_random = threshold.randomThresholding(img_gray, 0.3)
-io.imshow(bin_image_random)
+plt.figure(2)
+plt.imshow(bin_image_random, cmap='Greys_r')
+
