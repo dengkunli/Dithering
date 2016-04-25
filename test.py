fd8ac5d7e93bbd0bb3132c6d5c8050372963cedf
LE COQ ANTONY <antony.lecoq@free.fr>
2016-04-25 12:01:41 +0200

    ajout d'autres matrices

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from skimage import io, color
 import numpy
 
 def filtre(m,taille):
-    result = numpy.zeros(img_gray.shape)
+    result = numpy.zeros(taille)
     for i in range(taille[0]) :
         for j in range(taille[1]) :
             result[i][j] = m[i%m.shape[0]][j%m.shape[1]]
@@ -21,27 +21,46 @@ def ceuillage(imgQ, filtre):
                 result[i][j] = 1
     return result
 
-#matrice de ceuillage
-s = numpy.array([[34, 25, 21, 17, 29, 33],
-                 [30, 13, 9 , 5 , 12, 24],
-                 [18, 6 , 1 , 0 , 8 , 20],
-                 [22, 10, 2 , 3 , 4 , 16],
-                 [26, 14, 7 , 11, 15, 28],
-                 [35, 31, 19, 23, 27, 32]])
+def orderedThreshold(img,m):
+    #on charge l'image et on la transforme en niveau de gris
+    img = io.imread(img)
+    img_gray = color.rgb2gray(img)
+    
+    #on crée le filtre
+    fil = filtre(m,img_gray.shape)
+    
+    #on quantifie les niveux de gris de l'image et on effectue le ceuillage
+    imgQ = quantification(img_gray,m)
+    result = ceuillage(imgQ,fil)
+    
+    return result
 
-#on charge l'image et on la transforme en niveau de gris
-img = io.imread("Lenna.png")
-img_gray = color.rgb2gray(img)
+#Ordered threshold - Clustered dots
+s1 = numpy.array([[34, 25, 21, 17, 29, 33],
+                  [30, 13, 9 , 5 , 12, 24],
+                  [18, 6 , 1 , 0 , 8 , 20],
+                  [22, 10, 2 , 3 , 4 , 16],
+                  [26, 14, 7 , 11, 15, 28],
+                  [35, 31, 19, 23, 27, 32]])
+                 
+#Ordered matrix with central white point
+s2 = numpy.array([[34, 25, 21, 17, 29, 33],
+                  [30, 13, 9 , 5 , 12, 24],
+                  [18, 6 , 1 , 0 , 8 , 20],
+                  [22, 10, 2 , 3 , 4 , 16],
+                  [26, 14, 7 , 11, 15, 28],
+                  [35, 31, 19, 23, 27, 32]])
 
-#on crée le filtre
-filtre = filtre(s,img_gray.shape)
+#Ordered matrix with balanced centered point
+s3 = numpy.array([[30, 22, 16, 21, 33, 35],
+		       [24, 11, 7 , 9 , 26, 28],
+		       [13, 5 , 0 , 2 , 14, 19],
+		       [15, 3 , 1 , 4 , 12, 18],
+		       [27, 8 , 6 , 10, 25, 29],
+		       [32, 20, 17, 23, 31, 34]])                
         
 #ss = ss+0.5
 #D = 255./36.
-        
-#on quantifie les niveux de gris de l'image et on effectue le ceuillage
-imgQ = quantification(img_gray,s)
-result = ceuillage(imgQ,filtre)
-
+result = orderedThreshold("Lenna.png",s3)
 io.imshow(result)
 #io.show()
